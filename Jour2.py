acd51f3f77740852e06efcdfc25a49ed9f056a2e
Thibault Caron <thibault.caronvans@laposte.net>
2024-11-27 15:56:22 +0100

    Ajout Jour2-job4 and job5

--- Jour2.py
+++ Jour2.py
@@ -1,7 +1,7 @@
 # Job 1
 def Job1():
     i = 0
-    for i in range(20):
+    for i in range(21):
         print(i)
 
     return
@@ -22,18 +22,37 @@ def Job3():
     return
 
 
-# Job 4
+# Job 4 - tables de multiplications
 def Job4():
 
+    N = int(input("Veuillez entrer un entier N supérieur à zéro: "))
+    if N > 0:
+        for i in range(1, N+1):
+            print(f"La table de multiplication de {i} :")
+            for j in range(1, 11):
+                print(f"{i} X {j} = {i * j}")
+    else:
+        print("Veuillez entrer un nombre supérieur à zéro")
 
     return
 
 
-# Job 5
+# Job 5 - Boucle "while"
 def Job5():
 
+    N = int(input("Entrez un entier N supérieur à zéro : "))
+    if N > 0:
+        i = 0
+        while i <= N:
+            print(f"Table de multiplication de {i} :")
+            for j in range(1, 11):
+                print(f"{i} X {j} = {i * j}")
 
-    return
+            i += 1
+    else:
+        print("Veuillez entrer un nombre supérieur à zéro.")
+
+        return
 
 
 # Job 6
