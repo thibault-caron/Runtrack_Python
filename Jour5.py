ae8b1101155e93856c5bde9e3d0dca6b56bef54d
Thibault Caron <thibault.caronvans@laposte.net>
2024-11-28 14:57:55 +0100

    ajout Jour5 Job1

--- Jour5.py
+++ Jour5.py
@@ -1,8 +1,14 @@
-# Job 1
+# Job 1 - liste de fruits
 def Job1():
 
+    def get_fruits():
+        fruits = ["pomme", "cerise", "orange"]
+        return fruits
 
-    return
+    #apppel de la fonction et affichage du résultat
+    resultat = get_fruits()
+
+    return print(resultat)
 
 # Job 2
 def Job2():
@@ -65,3 +71,35 @@ def Job10():
 
     return
 
+# Job 11
+def Job11():
+
+
+    return
+
+
+# Job 12
+def Job12():
+
+
+    return
+
+# Job 13
+def Job13():
+
+
+    return
+
+# Job 14
+def Job14():
+
+
+    return
+
+# Job 15
+def Job15():
+
+
+    return
+
+Job1()
