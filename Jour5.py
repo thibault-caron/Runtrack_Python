97a4705c866686079bdd98de6ac4566831b9b9ad
Thibault Caron <thibault.caronvans@laposte.net>
2024-11-29 11:09:01 +0100

    add Jour5 Job15

--- Jour5.py
+++ Jour5.py
@@ -176,10 +176,20 @@ def Job14():
 
     return 
 
-# Job 15
+# Job 15 - arrondi les nombres round()
 def Job15():
+    L = [22.4, 4.0, 16.22, 9.10, 11.00, 12.22, 14.20, 5.20, 17.50]
+    print(f"liste initiale: {L}")
 
+    for i in range(len(L)):
+        L[i] = int(L[i] + 0.5)
+
+    for i in range(len(L)): 
+        for j in range(i+1, len(L)): 
+            if L[i] > L[j]: L[i], L[j] = L[j], L[i]
+    
+    print(f"liste finale: {L}")
 
     return
 
-Job14()
+Job15()
